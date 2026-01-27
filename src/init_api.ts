11ca0beeab3f2e7cfed52061bfc46fed27cc67dc
Marcus Nebel <marcusnebel09@gmail.com>
2026-01-27 08:13:30 +0100

    Update dependencies and improve API initialization
    Upgraded several dependencies in package.json, including @jimce-music/jimce-api-ts and electron-builder, and added new devDependencies for Electron packaging and signing. Also updated src/init_api.ts to use the latest API typings and initialization logic.

--- src/init_api.ts
+++ src/init_api.ts
@@ -2,9 +2,13 @@ import * as api from '@jimce-music/jimce-api-ts'
 
 // Set config
 api.setConfig({
-    baseUrl: 'http://localhost:8080',
+    baseUrl: 'http://192.168.188.27:8080',
     // You could also set default headers (maybe after the auth)
     // headers: {
     //     Authorization: 'Bearer <token>'
     // }
 })
+
+const pingRequest = api.getApiPing()
+
+console.log(pingRequest)
